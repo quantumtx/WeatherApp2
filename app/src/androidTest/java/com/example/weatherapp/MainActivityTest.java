4370604683dda0b2ad6777ce37835883186d909e
Quentin Williams <q.williams096@gmail.com>
2019-07-23 16:08:09 -0500

    basic app framework

---
+++ app/src/androidTest/java/com/example/weatherapp/MainActivityTest.java
@@ -0,0 +1,33 @@
+package com.example.weatherapp;
+
+
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Rule
+    public GrantPermissionRule mGrantPermissionRule =
+            GrantPermissionRule.grant(
+                    "android.permission.ACCESS_FINE_LOCATION",
+                    "android.permission.ACCESS_COARSE_LOCATION",
+                    "android.permission.INTERNET",
+                    "android.permission.ACCESS_NETWORK_STATE",
+                    "android.permission.SYSTEM_ALERT_WINDOW",
+                    "android.permission.WRITE_EXTERNAL_STORAGE");
+
+    @Test
+    public void mainActivityTest() {
+    }
+}
